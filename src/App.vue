87c0f79508d1e89054e7010fb7770821ca9ad0c7
Erick Costa <erickcosta2509@gmail.com>
2022-12-02 03:29:12 -0300

    estilização de navbar e footer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <RouterView />
+  <Footer />
+</template>
+
 <script>
 import { RouterView } from "vue-router"
 import Footer from "./components/Footer.vue"
@@ -5,13 +11,45 @@ import Navbar from "./components/Navbar.vue"
 export default {
   name: "App",
   components: { Navbar, Footer },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    }
+  },
 }
 </script>
 
-<template>
-  <Navbar />
-  <RouterView />
-  <Footer />
-</template>
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
-<style></style>
+body,
+html {
+  min-height: 100%;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav>
-    <RouterLink to="/">
-      <img src="" alt="logo" id="logo" />
+  <nav id="nav">
+    <RouterLink to="/" id="logo-url">
+      <img :src="logo" :alt="alt" id="logo" />
     </RouterLink>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/orders">Pedidos</RouterLink>
@@ -12,7 +12,38 @@
 import { RouterLink } from "vue-router"
 export default {
   name: "Navbar",
+  props: ["logo", "alt"],
 }
 </script>
 
-<style scope></style>
+<style scoped>
+#nav {
+  background-color: #222;
+  border-bottom: 4px solid #111;
+  padding: 15px 50px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+#nav #logo-url {
+  margin: auto;
+  margin-left: 0;
+}
+
+#logo {
+  width: 40px;
+  height: 40px;
+}
+
+#nav a {
+  color: #fcba03;
+  text-decoration: none;
+  margin: 12px;
+  transition: 0.5s;
+}
+
+#nav a:hover {
+  color: white;
+}
+</style>
