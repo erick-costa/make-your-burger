01895768b96172456cadf6bbbf7aa7a2813da254
Erick Costa <erickcosta2509@gmail.com>
2022-12-02 03:04:19 -0300

    configurando as rotas e layout básico

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <RouterLink to="/">
+      <img src="" alt="logo" id="logo" />
+    </RouterLink>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/orders">Pedidos</RouterLink>
+  </nav>
+</template>
+
+<script>
+import { RouterLink } from "vue-router"
+export default {
+  name: "Navbar",
+}
+</script>
+
+<style scope></style>
---
+++ src/views/Orders.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Pedidos</h1>
+  </div>
+</template>
+
+<style></style>
