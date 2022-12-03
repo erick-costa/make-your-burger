5b87b3ddf532edafc728e1a1d5edfc95ecd86745
Erick Costa <erickcosta2509@gmail.com>
2022-12-03 03:11:09 -0300

    atualizações dos pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -22,7 +22,11 @@
           </ul>
         </div>
         <div>
-          <select name="status" class="status">
+          <select
+            name="status"
+            class="status"
+            @change="updateBurger($event, burger.id)"
+          >
             <option value="">Selecione</option>
             <option
               v-for="s in status"
@@ -72,6 +76,17 @@ export default {
       const res = await req.json()
 
       this.getOrders()
+    },
+    async updateBurger(event, id) {
+      const option = event.target.value
+
+      const dataJson = JSON.stringify({ status: option })
+
+      const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: dataJson,
+      })
     },
   },
   mounted() {
