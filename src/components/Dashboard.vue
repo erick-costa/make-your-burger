fef754f42deb3a1cfba349d52bbfb9c70cdb2dba
Erick Costa <erickcosta2509@gmail.com>
2022-12-03 02:45:19 -0300

    buscando pedidos do banco

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,33 +11,14 @@
       </div>
     </div>
     <div id="burger-table-rows">
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão dabuioaf</div>
-        <div>Maminha</div>
+      <div class="burger-table-row" v-for="burger in burgers">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.nome }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
         <div>
           <ul>
-            <li>Salame</li>
-            <li>tomate</li>
-          </ul>
-        </div>
-        <div>
-          <select name="status" class="status">
-            <option value="">Selecione</option>
-          </select>
-          <button class="delete-btn">Cancelar</button>
-        </div>
-      </div>
-      <div class="burger-table-row">
-        <div class="order-number">1</div>
-        <div>João</div>
-        <div>Pão dabuioaf</div>
-        <div>Maminha</div>
-        <div>
-          <ul>
-            <li>Salame</li>
-            <li>tomate</li>
+            <li v-for="extra in burger.opcionais">{{ extra }}</li>
           </ul>
         </div>
         <div>
@@ -54,6 +35,25 @@
 <script>
 export default {
   name: "Dashboard",
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    }
+  },
+  methods: {
+    async getOrders() {
+      const req = await fetch("http://localhost:3000/burgers")
+      const data = await req.json()
+      this.burgers = data
+
+      console.log(this.burgers)
+    },
+  },
+  mounted() {
+    this.getOrders()
+  },
 }
 </script>
 
